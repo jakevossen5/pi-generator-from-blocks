84a25c668abe625f7644d242e3f039d29d53f319
Jake Vossen <jakevossen@jakevossen.codes>
2019-01-18 15:24:52 -0700

    Created initial variables

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut m1 = 1; // Mass of first (small) block
+    let mut m2 = 10; // Mass of the second (big) block
+
+    let mut v1 = 0; // velocity of the first (small) block
+    let mut v2 = -5; // velocity of second (big) block
+
+    let mut d = 5; // distance from the wall at collision?
+    println!("Hello world");
+}
